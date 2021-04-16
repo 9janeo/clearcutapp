8d856d94791eef0aee271a043de581464e7071ea
Niyi Adewole <9janeo@gmail.com>
2021-04-16 06:07:30 -0500

    Initial commit

---
+++ app/controllers/dash_controller.rb
@@ -0,0 +1,15 @@
+class DashController < ApplicationController
+    # skip_before_action :authenticate_user!, :only => [:index]
+    
+    def index
+        # if user_signed_in?
+            @user = User.find(1)
+            # @user_orders = @user.orders
+                # @user_keywords = @user.keywords
+            #redirect_to keywords_path
+        # else
+        # 	redirect_to new_user_session_url
+        # end
+    end
+
+end
