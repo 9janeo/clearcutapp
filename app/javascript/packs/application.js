d8d8b80dbbe8b53dcdceb1a4849f2db00bf56b95
Niyi Adewole <9janeo@gmail.com>
2021-05-09 11:17:50 -0500

    Disables tailwind, uses bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("bootstrap")
+
 import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-global.toastr = require("toastr")
+// global.toastr = require("toastr")
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
