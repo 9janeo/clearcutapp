28828599d7b3b43611eabd4170de2f1b72a504e3
Niyi Adewole <9janeo@gmail.com>
2021-10-31 14:55:48 -0500

    Add gulp and sass to front end build

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../stylesheets/application"
+
+// import "bootstrap/dist/css/bootstrap"
+// import "bootstrap/dist/js/bootstrap"
+
+//= require popper
+import "bootstrap"
+
+import "stylesheets/styles"
 
 Rails.start()
 Turbolinks.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,30 @@
 const { environment } = require('@rails/webpacker')
 
+// To get an overview, have a look at `environment.loaders`.
+// console.log(environment.loaders)
+
+// Get the actual sass-loader config
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function(element) {
+  return element.loader == 'sass-loader'
+})
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options
+options.implementation = require('sass')
+
+
+// Use custom js config in webpack
+const customConfig = require('./custom')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+environment.config.merge(customConfig)
+
 module.exports = environment
